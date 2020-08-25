0fae2d094b8efceddcbb83b30308088473e46606
Marcus Martins <marcusmartins38@gmail.com>
2020-08-25 19:16:46 -0400

    Saving the theme at localStorage

--- frontend/src/hooks/ThemeSwitch.tsx
+++ frontend/src/hooks/ThemeSwitch.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "styled-components";
 
 import light from "../styles/themes/light";
 import dark from "../styles/themes/dark";
+import usePersistedState from "../utils/usePersistedState";
 
 interface ThemeSwitchData {
   toggleTheme(): void;
@@ -11,7 +12,7 @@ interface ThemeSwitchData {
 export const ThemeSwitchContext = createContext({} as ThemeSwitchData);
 
 export const ThemeSwitchProvider: React.FC = ({ children }) => {
-  const [theme, setTheme] = useState(light);
+  const [theme, setTheme] = usePersistedState("theme", light);
 
   const toggleTheme = () => {
     setTheme(theme.title === "light" ? dark : light);
